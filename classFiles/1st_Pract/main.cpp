74e6dbeeb27b1c1dc39146d787a55008ffe7d573
ricrui3 <ricrui3@gmail.com>
2016-04-01 00:44:59 -0500

    main que genera una imagen con pixeles de color aleatorio sirve como prueba de las clases ImagenPPM y Pixel

---
+++ classFiles/1st_Pract/main.cpp
@@ -0,0 +1,18 @@
+#include "ImagePPM.h"
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  
+  if (argc != 3) {
+    cout << "\nERROR: La forma correcta de ejecutar el programa es: " << argv[0]
+         << " pixelesDeAncho pixelesDeAlto\n" << endl;
+    exit(0);
+  }
+
+  ImagePPM image(atoi(argv[1]), atoi(argv[2]));
+  //image.setRandomValues();
+  image.createImage();
+  return 0;
+}

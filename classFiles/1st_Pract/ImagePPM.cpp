7300d9f767dc0662566dd8d381f1a739aed2a33d
ricrui3 <ricrui3@gmail.com>
2016-04-01 00:42:04 -0500

    Archivos de la clase ImagenPPM funcionando

--- classFiles/1st_Pract/ImagePPM.cpp
+++ classFiles/1st_Pract/ImagePPM.cpp
@@ -1,1 +1,52 @@
+#include "ImagePPM.h"
+#include "Pixel.h"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+ImagePPM::ImagePPM(int width, int height) : dimX(width), dimY(height) {
+  backgroundColor.setRGB(200, 43, 90);
+  lineColor.setRGB(255, 255, 255);
+  for (int i = 0; i < dimX; i++) {
+    std::vector<Pixel> row;  // Create an empty row
+    for (int j = 0; j < dimY; j++) {
+      row.push_back(backgroundColor);  // Add an element (column) to the row
+    }
+    pixelMatrix.push_back(row);  // Add the row to the main vector
+  }
+}
+
+void ImagePPM::createImage() {
+  ofstream image;
+  image.open("Image.ppm", ios::out | ios::trunc);
+  image << "P3" << endl;
+  image << dimX << " " << dimY << endl;
+  image << "255" << endl;
+
+  for (int i = 0; i < dimX; ++i) {
+    for (int j = 0; j < dimY; ++j) {
+      image << pixelMatrix[i][j].colorR << " " << pixelMatrix[i][j].colorG
+            << " " << pixelMatrix[i][j].colorB << " ";
+    }
+    image << "\n";
+  }
+
+  image.close();
+  // system("eog Image.ppm");
+}
+
+void ImagePPM::setRandomValues() {
+  time_t t;
+
+  srand((unsigned)time(&t));
+
+  for (int i = 0; i < dimX; ++i) {
+    for (int j = 0; j < dimY; ++j) {
+      pixelMatrix[i][j].setRGB(rand() % 256, rand() % 256, rand() % 256);
+    }
+  }
+}
--- classFiles/1st_Pract/ImagePPM.h
+++ classFiles/1st_Pract/ImagePPM.h
@@ -1,33 +1,22 @@
 #ifndef IMAGE_PPM_H_
-#define IMAGE_PPM_H_ 
+#define IMAGE_PPM_H_
+#include "Pixel.h"
+#include <vector>
 
-class Image_PPM
-{
-private:
-	int dimX;
-	int dimY;
-	int background;
-public:
-	Image_PPM();
-	~Image_PPM();
-	int getDimX(){
-		return dimX;	
-	}
-	void setDimX(int dimx){
-		dimX = dimx;
-	}
-	int getDimY(){
-		return dimY;	
-	}
-	void setDimY(int dimy){
-		dimY = dimy;
-	}
-	int getBackground(){
-		return background;	
-	}
-	void setBackground(int bckg){
-		background = bckg;
-	}
+class ImagePPM {
+ private:
+  int dimX;
+  int dimY;
+  Pixel backgroundColor;
+  Pixel lineColor;
+
+ public:
+  std::vector<std::vector<Pixel> > pixelMatrix;
+  ImagePPM(int = 100, int = 100);
+  void createImage();
+  void setRandomValues();
+  void setBackgroundColor(Pixel bckgC) { backgroundColor = bckgC; }
+  void setLineColor(Pixel lineC) { lineColor = lineC; }
 };
 
 #endif

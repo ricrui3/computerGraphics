150df348a2ae3c8b789a1193ce84d12607c6e25e
ricrui3 <ricrui3@gmail.com>
2016-04-01 00:42:33 -0500

    Archivos de la clase Pixel funcionando

--- classFiles/1st_Pract/Pixel.h
+++ classFiles/1st_Pract/Pixel.h
@@ -2,21 +2,14 @@
 #define PIXEL_H_
 
 class Pixel {
- private:
+ public:
   int colorR;
   int colorG;
   int colorB;
-
- public:
   Pixel(int = 0, int = 0, int = 0);
   ~Pixel();
   void setRGB(int cR, int cG, int cB);
-  int getColorR() { return colorR; }
-  void setColorR(int color) { colorR = color; }
-  int getColorG() { return colorG; }
-  void setColorG(int color) { colorG = color; }
-  int getColorB() { return colorB; }
-  void setColorB(int color) { colorB = color; }
+  void setRGB(Pixel pixel);
 };
 
 #endif
